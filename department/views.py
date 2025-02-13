6fc1e018a7513e82492e5d90266b7da33aacc861
mikasa999 <143625524+mikasa999@users.noreply.github.com>
2025-02-13 17:56:32 +0800

    给部门app新增做了异步

--- department/views.py
+++ department/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Department
-from django.urls import reverse
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # 部门列表
 department_list_content = {
@@ -16,13 +17,19 @@ def department_list(request):
 
 
 # 部门添加
+@csrf_exempt
 def department_add(request):
-    if request.method == 'post':
-        department_name = request.POST.get('department_name')
-        department_code = request.POST.get('department_code')
-        Department.objects.create(department_name=department_name,department_code=department_code)
-        return redirect('department:department_list')
-        # 获取部门列表数据
-    else:
-        print('没有获取post值')
-        return redirect('leads:leads_list')
+    if request.method == 'POST':
+        try:
+            department_name = request.POST.get('department_name')
+            department_code = request.POST.get('department_code')
+
+            # 检查 department_code 是否已经存在
+            if Department.objects.filter(department_code=department_code).exists():
+                return JsonResponse({'message': f"部门编码 {department_code} 已经存在，请选择其他编码。"}, status=400)
+
+            Department.objects.create(department_name=department_name, department_code=department_code)
+            return JsonResponse({'message': '部门添加成功！'}, status=200)
+        except Exception as e:
+            return JsonResponse({'message': f'添加部门时出现错误: {str(e)}'}, status=500)
+    return JsonResponse({'message': '无效的请求方法'}, status=405)
